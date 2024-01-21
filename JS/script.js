f7a3b4bedd25b3b2558c026dd189128a0f9656dc
yungestAmine <purpleamine@gmail.com>
2024-01-21 15:50:53 +0100

    grafica ultimata

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,10 @@ let base = document.getElementsByClassName('home')
 let seeds = ['♤', '♡', '♢', '♧']
 for(let i=0; i<base.length; i++)
     base[i].innerHTML = getCard(13, seeds[i])
-
+let aside = document.getElementsByClassName('casellaAside')
+for(let i=0; i<aside.length; i++)   
+    aside[i].innerHTML = getBackCard()
+document.getElementById('mazzoUscito').innerHTML = getBackCard()
 function getCard(number, seed){
     return(
     `
@@ -12,8 +15,14 @@ function getCard(number, seed){
             <div class="cn">${number}</div>
             <div class="cs">${seed}</div>
             <div class="cbs">${seed}</div>
-            <div class="cs">${seed}</div>
-            <div class="cn">${number}</div>
+            <div class="cs" style="text-align: right; padding-left:0; margin-right: 12px;">${seed}</div>
+            <div class="cn" style="text-align: right; padding-left:0; margin-right: 12px;">${number}</div>
         </div>
     `)
+}
+function getBackCard(){
+    return(
+        `
+            <div class="cpn">?</div>
+        `)
 }
